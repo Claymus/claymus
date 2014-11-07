3c738cb30fe2db6f08a159b5e010bd62f76a6c5e
Sunny Kumar <sunny.genious@gmail.com>
2014-11-07 09:10:18 +0530

    Comment Status Added

---
+++ src/com/claymus/commons/shared/CommentStatus.java
@@ -0,0 +1,11 @@
+package com.claymus.commons.shared;
+
+import com.google.gwt.user.client.rpc.IsSerializable;
+
+public enum CommentStatus implements IsSerializable{
+
+	APPROVED,
+	REJECTED,
+	PENDING_APPROVAL
+
+}
