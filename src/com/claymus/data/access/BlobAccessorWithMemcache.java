8a4940cd55ca9a08a6a44c7f176476ffa16dcec5
Rahul Ranjan <genirahul@gmail.com>
2014-12-19 22:25:46 +0530

    changes in BlobAccessorWithMemcache file

--- src/com/claymus/data/access/BlobAccessorWithMemcache.java
+++ src/com/claymus/data/access/BlobAccessorWithMemcache.java
@@ -4,18 +4,26 @@ import java.io.IOException;
 import java.nio.charset.Charset;
 import java.util.List;
 import java.util.Map;
+import java.util.logging.Level;
+import java.util.logging.Logger;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
 
+import com.claymus.commons.server.ResizeBlob;
+import com.claymus.data.access.gcs.BlobEntryGcsImpl;
 import com.claymus.data.transfer.BlobEntry;
 
 public class BlobAccessorWithMemcache implements BlobAccessor {
 
 	private final static String PREFIX = "BlobEntry-";
+	private final static String SUFFIX = "-Segment-";
 
 	private final BlobAccessor blobAccessor;
 	private final Memcache memcache;
+	
+	private static final Logger logger = 
+			Logger.getLogger( BlobAccessorWithMemcache.class.getName() );
 
 
 	public BlobAccessorWithMemcache( BlobAccessor blobAccessor, Memcache memcache ) {
@@ -82,11 +90,44 @@ public class BlobAccessorWithMemcache implements BlobAccessor {
 			throws IOException {
 		
 		BlobEntry blobEntry = memcache.get( PREFIX + fileName );
-		if( blobEntry == null ) {
+		Long totalNoOfSegments = 0L;
+		int segmentNo = 1;
+
+		if( blobEntry != null && blobEntry.getSize() >= 1000 * 1024 ){
+			ResizeBlob resizeBlob = new ResizeBlob( blobEntry );
+			totalNoOfSegments = resizeBlob.getSegmentCount();
+			while( (long) segmentNo < totalNoOfSegments ){
+				byte[] temp = memcache.get( PREFIX + fileName + SUFFIX + segmentNo );
+				if( temp == null )
+					break;
+				resizeBlob.appendSegment( temp );
+				segmentNo++;
+			}
+			blobEntry = resizeBlob.getBlobEntry();
+			logger.log( Level.INFO, "Cache Hit : Blob Length - " + blobEntry.getData().length );
+		}
+		
+		if( blobEntry == null || segmentNo < totalNoOfSegments ) {
 			blobEntry = blobAccessor.getBlob( fileName );
+			logger.log( Level.INFO, "Cache Miss : Blob length - " + blobEntry.getData().length );
 			if( blobEntry != null && blobEntry.getData().length <= 1000 * 1024 )
 				memcache.put( PREFIX + fileName, blobEntry );
+			else if( blobEntry != null && blobEntry.getData().length > 1000 * 1024 ) {
+				BlobEntry temp = new BlobEntryGcsImpl( (BlobEntryGcsImpl) blobEntry );
+				ResizeBlob resizeBlob = new ResizeBlob( temp );
+				totalNoOfSegments = resizeBlob.getSegmentCount();
+											
+				memcache.put( PREFIX + fileName, resizeBlob.getMemcacheOptimizedBlob() );
+				
+				List<byte[]> segmentList = resizeBlob.getSegmentList();
+				segmentNo = 1;
+				for( byte[] segment : segmentList ){
+					memcache.put( PREFIX + fileName + SUFFIX + segmentNo, segment );
+					segmentNo++;
+				}
+			}
 		}
+		logger.log( Level.INFO, "Final Blob Length : " + blobEntry.getData().length );
 		return blobEntry;
 	}
 	
