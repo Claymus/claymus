bc5c54918939321cbbe7c5bcdf22a9b648b19a3a
Rahul Ranjan <genirahul@gmail.com>
2015-02-10 23:53:08 +0530

    Made filebrowserEntity persistable

--- src/com/claymus/pagecontent/filebrowser/gae/FileBrowserEntity.java
+++ src/com/claymus/pagecontent/filebrowser/gae/FileBrowserEntity.java
@@ -1,8 +1,11 @@
 package com.claymus.pagecontent.filebrowser.gae;
 
+import javax.jdo.annotations.PersistenceCapable;
+
 import com.claymus.data.access.gae.PageContentEntity;
 import com.claymus.pagecontent.filebrowser.FileBrowser;
 
 @SuppressWarnings("serial")
+@PersistenceCapable
 public class FileBrowserEntity extends PageContentEntity implements FileBrowser {
 }
