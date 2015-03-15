3bb73cf6e97ae21388146187097cafb592a25c07
Prashant Gupta <antshpra@gmail.com>
2015-03-15 22:47:01 +0530

    Bulk add to TaskQueue.

--- src/com/claymus/taskqueue/TaskQueue.java
+++ src/com/claymus/taskqueue/TaskQueue.java
@@ -1,7 +1,11 @@
 package com.claymus.taskqueue;
 
+import java.util.List;
+
 public interface TaskQueue {
 
 	void add( Task task );
+
+	void add( List<Task> taskList );
 	
 }
--- src/com/claymus/taskqueue/TaskQueueGaeImpl.java
+++ src/com/claymus/taskqueue/TaskQueueGaeImpl.java
@@ -1,7 +1,11 @@
 package com.claymus.taskqueue;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import com.google.appengine.api.taskqueue.Queue;
 import com.google.appengine.api.taskqueue.QueueFactory;
+import com.google.appengine.api.taskqueue.TaskOptions;
 
 public class TaskQueueGaeImpl implements TaskQueue {
 
@@ -12,9 +16,18 @@ public class TaskQueueGaeImpl implements TaskQueue {
 		this.queue = QueueFactory.getQueue( taskQueueName );
 	}
 
-	
+
+	@Override
 	public void add( Task task ) {
-		queue.add( ((TaskGaeImpl) task).getTaskOptions() );
+		queue.add( ( (TaskGaeImpl) task ).getTaskOptions() );
+	}
+	
+	@Override
+	public void add( List<Task> taskList ) {
+		List<TaskOptions> taskOptionsList = new ArrayList<TaskOptions>( taskList.size() );
+		for( Task task : taskList )
+			taskOptionsList.add( ( (TaskGaeImpl) task ).getTaskOptions() );
+		queue.add( taskOptionsList );
 	}
 	
 }
