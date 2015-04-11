bb55e3824a78f96ef14b24ed8f43e0917206b752
Rahul Ranjan <genirahul@gmail.com>
2015-04-12 02:10:36 +0530

    pratilipi.info for static content

--- src/com/claymus/commons/shared/ClaymusResource.java
+++ src/com/claymus/commons/shared/ClaymusResource.java
@@ -2,44 +2,44 @@ package com.claymus.commons.shared;
 
 public enum ClaymusResource implements Resource {
 
-	CKEDITOR( "<script src='//cdn-asia.pratilipi.com/third-party/ckeditor-4.4.5-full/ckeditor.js' charset='utf-8'></script>" ),
+	CKEDITOR( "<script src='//c.pratilipi.info/third-party/ckeditor-4.4.5-full/ckeditor.js' charset='utf-8'></script>" ),
 
 	JQUERY_1( "" ),
-	JQUERY_2( "<script src='//ajax.googleapis.com/ajax/libs/jquery/2.1.3/jquery.min.js'></script>" ),
+	JQUERY_2( "<script src='//j.pratilipi.com/third-party/jquery/2.1.3/jquery.min.js'></script>" ),
 
-	BOOTSTRAP( "<link rel='stylesheet' href='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/css/bootstrap.min.css'>"
-			+ "<script src='//maxcdn.bootstrapcdn.com/bootstrap/3.3.4/js/bootstrap.min.js'></script>" ),
+	BOOTSTRAP( "<link rel='stylesheet' href='//b.pratilipi.com/third-party/bootstrap/3.3.4/css/bootstrap.min.css'>"
+			+ "<script src='//b.pratilipi.com/third-party/bootstrap/3.3.4/js/bootstrap.min.js'></script>" ),
 
-	POLYMER( "<script src='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/webcomponentsjs/webcomponents.js'></script>"
-			+ "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/polymer/polymer.html'>" ),
+	POLYMER( "<script src='//p.pratilipi.info/third-party/polymer-0.5.5/webcomponentsjs/webcomponents.js'></script>"
+			+ "<link rel='import' href='//p.pratilipi.info/third-party/polymer-0.5.5/polymer/polymer.html'>" ),
 
-	POLYMER_CORE_AJAX				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-ajax/core-ajax.html'>" ),
-	POLYMER_CORE_COLLAPSE			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-collapse/core-collapse.html'>" ),
-	POLYMER_CORE_DRAWER_PANEL		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-drawer-panel/core-drawer-panel.html'>" ),
-	POLYMER_CORE_DROPDOWN_MENU		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-dropdown-menu/core-dropdown-menu.html'>" ),
-	POLYMER_CORE_HEADER_PANEL		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-header-panel/core-header-panel.html'>" ),
-	POLYMER_CORE_ICON_BUTTON		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-icon-button/core-icon-button.html'>" ),
-	POLYMER_CORE_ICONS				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-icons/core-icons.html'>" ),
-	POLYMER_CORE_ITEM				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-item/core-item.html'>" ),
-	POLYMER_CORE_MENU				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-menu/core-menu.html'>" ),
-	POLYMER_CORE_SCROLL_HEADER_PANEL( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-scroll-header-panel/core-scroll-header-panel.html'>" ),
-	POLYMER_CORE_SCROLL_THREHSOLD	( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-scroll-threshold/core-scroll-threshold.html'>" ),
-	POLYMER_CORE_TOOLBAR			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/core-toolbar/core-toolbar.html'>" ),
+	POLYMER_CORE_AJAX				( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-ajax/core-ajax.html'>" ),
+	POLYMER_CORE_COLLAPSE			( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-collapse/core-collapse.html'>" ),
+	POLYMER_CORE_DRAWER_PANEL		( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-drawer-panel/core-drawer-panel.html'>" ),
+	POLYMER_CORE_DROPDOWN_MENU		( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-dropdown-menu/core-dropdown-menu.html'>" ),
+	POLYMER_CORE_HEADER_PANEL		( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-header-panel/core-header-panel.html'>" ),
+	POLYMER_CORE_ICON_BUTTON		( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-icon-button/core-icon-button.html'>" ),
+	POLYMER_CORE_ICONS				( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-icons/core-icons.html'>" ),
+	POLYMER_CORE_ITEM				( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-item/core-item.html'>" ),
+	POLYMER_CORE_MENU				( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-menu/core-menu.html'>" ),
+	POLYMER_CORE_SCROLL_HEADER_PANEL( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-scroll-header-panel/core-scroll-header-panel.html'>" ),
+	POLYMER_CORE_SCROLL_THREHSOLD	( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-scroll-threshold/core-scroll-threshold.html'>" ),
+	POLYMER_CORE_TOOLBAR			( "<link rel='import' href='//pc.pratilipi.info/third-party/polymer-0.5.5/core-toolbar/core-toolbar.html'>" ),
 	
-	POLYMER_PAPER_ACTION_DIALOG		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-dialog/paper-action-dialog.html'>" ),
-	POLYMER_PAPER_BUTTON			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-button/paper-button.html'>" ),
-	POLYMER_PAPER_DIALOG			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-dialog/paper-dialog.html'>" ),
-	POLYMER_PAPER_DROPDOWN			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-dropdown/paper-dropdown.html'>" ),
-	POLYMER_PAPER_DROPDOWN_MENU		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-dropdown-menu/paper-dropdown-menu.html'>" ),
-	POLYMER_PAPER_FAB				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-fab/paper-fab.html'>" ),
-	POLYMER_PAPER_ICON_BUTTON		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-icon-button/paper-icon-button.html'>" ),
-	POLYMER_PAPER_INPUT				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-input/paper-input.html'>" ),
-	POLYMER_PAPER_INPUT_DECORATOR	( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-input/paper-input-decorator.html'>" ),
-	POLYMER_PAPER_ITEM				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-item/paper-item.html'>" ),
-	POLYMER_PAPER_MENU_BUTTON		( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-menu-button/paper-menu-button.html'>" ),
-	POLYMER_PAPER_SLIDER			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-slider/paper-slider.html'>" ),
-	POLYMER_PAPER_SPINNER			( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-spinner/paper-spinner.html'>" ),
-	POLYMER_PAPER_TOAST				( "<link rel='import' href='//cdn-asia.pratilipi.com/third-party/polymer-0.5.5/paper-toast/paper-toast.html'>" ),
+	POLYMER_PAPER_ACTION_DIALOG		( "<link rel='import' href='//cdn-asia.pratilipi.info/third-party/polymer-0.5.5/paper-dialog/paper-action-dialog.html'>" ),
+	POLYMER_PAPER_BUTTON			( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-button/paper-button.html'>" ),
+	POLYMER_PAPER_DIALOG			( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-dialog/paper-dialog.html'>" ),
+	POLYMER_PAPER_DROPDOWN			( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-dropdown/paper-dropdown.html'>" ),
+	POLYMER_PAPER_DROPDOWN_MENU		( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-dropdown-menu/paper-dropdown-menu.html'>" ),
+	POLYMER_PAPER_FAB				( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-fab/paper-fab.html'>" ),
+	POLYMER_PAPER_ICON_BUTTON		( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-icon-button/paper-icon-button.html'>" ),
+	POLYMER_PAPER_INPUT				( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-input/paper-input.html'>" ),
+	POLYMER_PAPER_INPUT_DECORATOR	( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-input/paper-input-decorator.html'>" ),
+	POLYMER_PAPER_ITEM				( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-item/paper-item.html'>" ),
+	POLYMER_PAPER_MENU_BUTTON		( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-menu-button/paper-menu-button.html'>" ),
+	POLYMER_PAPER_SLIDER			( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-slider/paper-slider.html'>" ),
+	POLYMER_PAPER_SPINNER			( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-spinner/paper-spinner.html'>" ),
+	POLYMER_PAPER_TOAST				( "<link rel='import' href='//pp.pratilipi.info/third-party/polymer-0.5.5/paper-toast/paper-toast.html'>" ),
 	;
 	
 
