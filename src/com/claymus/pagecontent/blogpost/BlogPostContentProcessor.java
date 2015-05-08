f22823f025a2af34089d6dee1b4f0a12867ecda0
Rahul Ranjan <genirahul@gmail.com>
2015-05-08 20:12:42 +0530

    facebook og tags in blog post page

--- src/com/claymus/pagecontent/blogpost/BlogPostContentProcessor.java
+++ src/com/claymus/pagecontent/blogpost/BlogPostContentProcessor.java
@@ -6,14 +6,47 @@ import java.util.Map;
 import javax.servlet.http.HttpServletRequest;
 
 import com.claymus.commons.server.ClaymusHelper;
+import com.claymus.commons.server.FacebookApi;
 import com.claymus.commons.server.FreeMarkerUtil;
+import com.claymus.commons.shared.Resource;
 import com.claymus.commons.shared.exception.UnexpectedServerException;
 import com.claymus.pagecontent.PageContentProcessor;
 import com.claymus.pagecontent.PageContentRegistry;
 import com.claymus.pagecontent.blogpost.shared.BlogPostContentData;
 
 public class BlogPostContentProcessor extends PageContentProcessor<BlogPostContent> {
+	
+	private static final String DOMAIN = ClaymusHelper.getSystemProperty( "domain" );
 
+	@Override
+	public Resource[] getDependencies( BlogPostContent blogPostContent, HttpServletRequest request){
+	
+		String ogFbAppId = FacebookApi.getAppId( request );
+		String ogLocale = "hi_IN";
+		String ogTitle = blogPostContent.getTitle();
+		String ogUrl = "http://" + DOMAIN + "/blog/" + blogPostContent.getId();
+		String ogPublisher = "https://www.facebook.com/Pratilipidotcom";
+		
+		final String fbOgTags = "<meta property='fb:app_id' content='" + ogFbAppId + "' />"
+				+ "<meta property='og:locale' content='" + ogLocale + "' />"
+				+ "<meta property='og:type' content='website' />"
+				+ "<meta property='og:title' content='" + ogTitle + "' />"
+				+ "<meta property='og:url' content='" + ogUrl + "' />"
+				+ "<meta property='og:publisher' content='" + ogPublisher + "' />";
+		
+		
+		return new Resource[] {
+
+			new Resource() {
+				@Override
+				public String getTag() {
+					return fbOgTags;
+				}
+			},
+
+		};
+	}
+	
 	@Override
 	protected CacheLevel getCacheLevel( BlogPostContent blogPostContent, HttpServletRequest request ) {
 		return CacheLevel.USER_ROLE;
