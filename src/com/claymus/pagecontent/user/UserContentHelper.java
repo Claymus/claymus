af9977c8d85aa1ffd5175b245d0fb38b92428a77
Rahul Ranjan <genirahul@gmail.com>
2015-06-15 19:56:48 +0530

    isAdmin functin added to UserContentHelper

--- src/com/claymus/pagecontent/user/UserContentHelper.java
+++ src/com/claymus/pagecontent/user/UserContentHelper.java
@@ -1,9 +1,14 @@
 package com.claymus.pagecontent.user;
 
+import java.util.List;
+
 import javax.servlet.http.HttpServletRequest;
 
 import com.claymus.commons.server.Access;
 import com.claymus.commons.server.ClaymusHelper;
+import com.claymus.data.access.DataAccessorFactory;
+import com.claymus.data.transfer.AccessToken;
+import com.claymus.data.transfer.UserRole;
 import com.claymus.pagecontent.PageContentHelper;
 import com.claymus.pagecontent.user.gae.UserContentEntity;
 import com.claymus.pagecontent.user.shared.UserContentData;
@@ -38,6 +43,18 @@ public class UserContentHelper extends PageContentHelper<
 		return new UserContentEntity();
 	}
 
+	public static boolean isAdmin( HttpServletRequest request ){
+		
+		AccessToken accessToken = ( AccessToken ) request.getAttribute( ClaymusHelper.REQUEST_ATTRIB_ACCESS_TOKEN );
+		List<UserRole> userRoleList = DataAccessorFactory.getDataAccessor( request )
+											.getUserRoleList( accessToken.getUserId() );
+		for( UserRole userRole : userRoleList ){
+			if( userRole.getRoleId().equals( "administrator" ))
+				return true;
+		}
+		
+		return false;
+	}
 	
 	public static boolean hasRequestAccessToListUserData( HttpServletRequest request ) {
 		return ClaymusHelper.get( request ).hasUserAccess( ACCESS_TO_LIST_USER_DATA );
